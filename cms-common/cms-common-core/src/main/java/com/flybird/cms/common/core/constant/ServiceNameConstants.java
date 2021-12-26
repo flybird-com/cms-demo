b6b0ec8f8d64be7f4dd35e03ce6918fe119e7e68
kevin <kevin_love_it@163.com>
2021-12-26 15:11:44 +0800

    add common-core files.

---
+++ cms-common/cms-common-core/src/main/java/com/flybird/cms/common/core/constant/ServiceNameConstants.java
@@ -0,0 +1,24 @@
+package com.flybird.cms.common.core.constant;
+
+/**
+ * description: 服务名称
+ *
+ * @author: flybird
+ * @date: 2021-12-26 14:44:01
+ */
+public class ServiceNameConstants {
+    /**
+     * 认证服务的serviceid
+     */
+    public static final String AUTH_SERVICE = "cms-auth";
+
+    /**
+     * 系统模块的serviceid
+     */
+    public static final String SYSTEM_SERVICE = "cms-system";
+
+    /**
+     * 文件服务的serviceid
+     */
+    public static final String FILE_SERVICE = "cms-file";
+}
