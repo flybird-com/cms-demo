0113bb8a986f2ee015b4665334d1b4abaca73c31
kevin <kevin_love_it@163.com>
2022-01-01 16:19:10 +0800

    finish cms-common-swagger.

---
+++ cms-common/cms-common-swagger/src/main/java/com/flybird/cms/common/swagger/annotation/EnableCustomSwagger2.java
@@ -0,0 +1,15 @@
+package com.flybird.cms.common.swagger.annotation;
+
+import com.flybird.cms.common.swagger.config.SwaggerAutoConfiguration;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+@Import({SwaggerAutoConfiguration.class})
+public @interface EnableCustomSwagger2 {
+
+}
